d3e4c86d8fe5379e113a5411a8c2ec3a1b8983da
Anant Jain <ritujainanant2810@gmail.com>
2024-07-15 13:58:43 +0530

    constants updated

--- Context/constants.js
+++ Context/constants.js
@@ -10,8 +10,8 @@ export const ERC20Generator_BYTECODE = ERC20Generator.bytecode;
 export const ICO_MARKETPLACE_ADDRESS = process.env.NEXT_PUBLIC_ICO_MARKETPLACE_ADDRESS;
 export const ICO_MARKETPLACE_ABI = icoMarketplace.abi;
 
-export const PINATA_API_KEY = process.env.NEXT_PUBLIC_PINATA_AIP_KEY;
-export const PINATA_SECRET_KEY = process.env.NEXT_PUBLIC_PINATA_SECRECT_KEY;
+export const PINATA_API_KEY = process.env.NEXT_PUBLIC_PINATA_API_KEY;
+export const PINATA_SECRET_KEY = process.env.NEXT_PUBLIC_PINATA_SECRET_KEY;
 
 //Networks
 
@@ -38,4 +38,90 @@ const networks = {
         rpcUrls: ["https://rpc.ankr.com/polygon"],
         blockExplorerUrls: ["https://polyscan.com/"]
     },
+    bsc: {
+        chainId: `0x${Number(56).toString(16)}`,
+        chainName: "Binance Mainnet",
+        nativeCurrency: {
+            name: "Binance Chain",
+            symbol: "BNB",
+            decimals: 18,
+        },
+        rpcUrls: ["https://rpc.ankr.com/bsc"],
+        blockExplorerUrls: ["https://bscscan.com/"]
+    },
+    base: {
+        chainId: `0x${Number(8453).toString(16)}`,
+        chainName: "Base Mainnet",
+        nativeCurrency: {
+            name: "ETH",
+            symbol: "ETH",
+            decimals: 18,
+        },
+        rpcUrls: ["https://mainnet.base.org"],
+        blockExplorerUrls: ["https://bscscan.com/"]
+    },
+};
+
+const changeNetwork = async({networkName}) => {
+    try {
+        if(!window.ethereum) throw new Error("No crypto Wallet found");
+        await window.ethereum.request({
+            method: "wallet_addEthereumChain",
+            params: [
+                {
+                    ...networks[networkName],
+                },
+            ]
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const handleNetworkChange = async() => {
+    const networkName = "polygon_amoy";
+    await changeNetwork({networkName});
+}
+
+export const shortenAddress = (address) => `${address?.slice(0,5)} ...${address?.length-4}`;
+
+const fetchContract = (address, abi, signer) => new ethers.Contract(address, abi, signer);
+
+export const ICO_MARKETPLACE_CONTRACT = async() => {
+    try {
+        const web3Modal = new web3Modal();
+        const connection = await web3Modal.connect();
+        const provider = new ethers.providers.Web3Provider(connection);
+
+        const signer = provider.getSigner();
+
+        const contract = fetchContract(
+            ICO_MARKETPLACE_ADDRESS,
+            ICO_MARKETPLACE_ABI,
+            signer
+        );
+
+        return contract;
+    } catch (error) {
+        console.log(error);
+    }
+}
+export const TOKEN_CONTRACT = async(TOKEN_ADDRESS) => {
+    try {
+        const web3Modal = new web3Modal();
+        const connection = await web3Modal.connect();
+        const provider = new ethers.providers.Web3Provider(connection);
+
+        const signer = provider.getSigner();
+
+        const contract = fetchContract(
+            TOKEN_ADDRESS,
+            ERC20GeneratorABI,
+            signer
+        );
+
+        return contract;
+    } catch (error) {
+        console.log(error);
+    }
 }
